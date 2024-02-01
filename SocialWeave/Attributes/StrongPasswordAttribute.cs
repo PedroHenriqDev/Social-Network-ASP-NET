f8ef0735cf9f60c230e0f56ebbf965f2c9f49d78
Pedro Henirique <heroperde@gmail.com>
2024-02-01 19:59:26 -0300

    Implement custom attribute StrongPasswordAttribute

---
+++ SocialWeave/Attributes/StrongPasswordAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialWeave.Attributes
+{
+    public class StrongPasswordAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if (value == null) return false;
+
+            string password = (string)value;
+            bool hasCapital = password.Any(char.IsUpper);
+            bool hasTiny = password.Any(char.IsLower);
+            bool hasNumber = password.Any(char.IsNumber);
+            bool hasLetter = password.Any(char.IsLetter);
+
+            return hasCapital && hasTiny && hasNumber && hasLetter;
+
+        }
+    }
+}
