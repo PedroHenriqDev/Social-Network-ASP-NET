ac5e633b9e5509ae9d189be99117c842a9ba04da
Pedro Henirique <heroperde@gmail.com>
2024-02-08 20:04:36 -0300

    Implemented logic to show post duration

---
+++ SocialWeave/Extensions/DateTimeExtension.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace SocialWeave.Extensions
+{
+    static public class DateTimeExtension
+    {
+        public static string Duration(this DateTime thisDateTime) 
+        {
+            if(thisDateTime == null || !(thisDateTime is DateTime))
+            {
+                return "0 min";
+            }
+
+            TimeSpan duration = DateTime.Now.Subtract(thisDateTime);
+
+            if (duration.TotalMinutes < 60) 
+            {
+                return duration.TotalMinutes.ToString("F1", CultureInfo.InvariantCulture) + " min";
+            }
+            else if(duration.TotalHours < 24) 
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " h";
+            }
+            else
+            {
+                DateTime nextMonth = thisDateTime.AddMonths(1);
+                TimeSpan daysInMonth = nextMonth.AddDays(-nextMonth.Day).Subtract(thisDateTime.AddDays(-thisDateTime.Day));
+
+                if (duration.TotalDays < daysInMonth.TotalDays)
+                {
+                    return ((int)duration.TotalDays).ToString("F1", CultureInfo.InvariantCulture) + " d";
+                }
+                else
+                {
+                    return thisDateTime.Year.ToString("F1", CultureInfo.InvariantCulture) + " year";
+                }
+            }
+        }
+    }
+}
