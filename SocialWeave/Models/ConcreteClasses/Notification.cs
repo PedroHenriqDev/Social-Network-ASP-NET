b4f3c4f3e5a15079e0e605ee095ab35839ea57f3
Pedro Henirique <heroperde@gmail.com>
2024-02-22 20:47:57 -0300

    Created notification object

---
+++ SocialWeave/Models/ConcreteClasses/Notification.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace SocialWeave.Models.ConcreteClasses
+{
+    public class Notification
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public string Date {  get; set; }
+
+        [Required]
+        public bool WasSeen { get; set; }
+    }
+}
