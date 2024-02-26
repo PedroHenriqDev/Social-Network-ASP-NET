f0c6e6320b61e498decf00439e0e520578c1dcd7
Pedro Henirique <heroperde@gmail.com>
2024-02-26 18:54:08 -0300

    Documented IProfilePictureService interface

--- SocialWeave/Models/Interfaces/IProfilePictureService.cs
+++ SocialWeave/Models/Interfaces/IProfilePictureService.cs
@@ -1,7 +1,18 @@
-﻿namespace SocialWeave.Models.Interfaces
+﻿using System.Threading.Tasks;
+
+namespace SocialWeave.Models.Interfaces
 {
+    /// <summary>
+    /// Interface for managing profile pictures.
+    /// </summary>
     public interface IProfilePictureService
     {
+        /// <summary>
+        /// Saves the profile picture asynchronously.
+        /// </summary>
+        /// <param name="pictureData">The byte array representing the picture data.</param>
+        /// <param name="webRootPath">The root path of the web application.</param>
+        /// <returns>The URL of the saved profile picture.</returns>
         Task<string> SaveProfilePictureAsync(byte[] pictureData, string webRootPath);
     }
 }
