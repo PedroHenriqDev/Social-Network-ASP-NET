5134643a5c069cc43ef125541f201bc1c2dd5a7b
Pedro Henirique <heroperde@gmail.com>
2024-02-26 19:06:35 -0300

    Documentation in context

--- SocialWeave/Data/ApplicationDbContext.cs
+++ SocialWeave/Data/ApplicationDbContext.cs
@@ -5,28 +5,66 @@ using SocialWeave.Models.ConcreteClasses;
 
 namespace SocialWeave.Data
 {
+    /// <summary>
+    /// Represents the application's database context, providing access to the database tables.
+    /// </summary>
     public class ApplicationDbContext : DbContext
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApplicationDbContext"/> class.
+        /// </summary>
+        /// <param name="options">The options for configuring the DbContext.</param>
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
 
+        /// <summary>
+        /// Default constructor for ApplicationDbContext.
+        /// </summary>
         public ApplicationDbContext()
         {
         }
 
+        /// <summary>
+        /// Represents the Users table in the database.
+        /// </summary>
         public DbSet<User> Users { get; set; }
+
+        /// <summary>
+        /// Represents the Notifications table in the database.
+        /// </summary>
         public DbSet<Notification> Notifications { get; set; }
+
+        /// <summary>
+        /// Represents the Posts table in the database.
+        /// </summary>
         public DbSet<Post> Posts { get; set; }
+
+        /// <summary>
+        /// Represents the Likes table in the database.
+        /// </summary>
         public DbSet<Like> Likes { get; set; }
+
+        /// <summary>
+        /// Represents the Admirations table in the database.
+        /// </summary>
         public DbSet<Admiration> Admirations { get; set; }
+
+        /// <summary>
+        /// Represents the Comments table in the database.
+        /// </summary>
         public DbSet<Comment> Comments { get; set; }
 
+        /// <summary>
+        /// Configures the entity models and relationships for the database.
+        /// </summary>
+        /// <param name="modelBuilder">The model builder used to configure the database.</param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+            // Configure the Notification entity
             modelBuilder.Entity<Notification>()
                 .HasKey(c => c.Id);
 
@@ -44,11 +82,13 @@ namespace SocialWeave.Data
                 .IsRequired(false)
                 .OnDelete(DeleteBehavior.Restrict);
 
+            // Configure the Post entity
             modelBuilder.Entity<Post>()
                 .HasDiscriminator<string>("PostType")
                 .HasValue<PostWithoutImage>("PostWithoutImage")
                 .HasValue<PostWithImage>("PostWithImage");
 
+            // Configure the Admiration entity
             modelBuilder.Entity<Admiration>()
                 .HasKey(c => c.Id);
 
